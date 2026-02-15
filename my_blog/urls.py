117e13254266abab6d7ed0f868cbd8ff476167bf
AlexeyDemidow <usernzt@gmail.com>
2026-02-15 15:39:37 +0100

    Clean up code in my_blog/urls.py

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,22 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 from posts.views import PostList
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),  # Главная
     path('admin/', admin.site.urls),
-    # path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('posts/', include('posts.urls')),
     path('chat/', include('chat.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('accounts/', include("allauth.urls")),
-
 
 ]
 
