ebf2dda484471c483719da6d42f9b7f4b90c6d14
AlexeyDemidow <usernzt@gmail.com>
2026-02-15 15:05:21 +0100

    Clean up code in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,20 @@
-from django.conf import Settings
 from django.urls import path
-
-from users.views import SignUpView, SignUpSuccess, Login, Profile, UpdateProfile, ProfileSettings, ProfileSearch, \
-    profile_search, toggle_follow, Subscriptions, Subscribers, subscribers_search, subscriptions_search
 from django.contrib.auth import views
 
+from users.views import *
+
 urlpatterns = [
-    path('<int:pk>/', Profile.as_view(), name='profile'),  # Профиль
+    path('<int:pk>/', Profile.as_view(), name='profile'),
     path('settings/', ProfileSettings.as_view(), name='settings'),
-    path('<int:pk>/update_profile/', UpdateProfile.as_view(), name='update_profile'),  # Обновление данных профиля
+    path('<int:pk>/update_profile/', UpdateProfile.as_view(), name='update_profile'),
     path('<int:pk>/follow/', toggle_follow, name='toggle_follow'),
-    path('signup/', SignUpView.as_view(), name='account_signup'),  # Регистрация
-    path('signup_success/', SignUpSuccess.as_view(), name='signup_success'),  # Сигнал об успешной регистрации
-    path('login/', Login.as_view(), name='account_login'),  # Вход
-    path('logout/', views.LogoutView.as_view(), name='logout'),  # Выход
-    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),  # Сброс пароля
-    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),  # Сигнал об успешном сбросе пароля
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),  # Подтверждение сброса пароля
-    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),  # Сброс пароля успешен
+    path('signup/', SignUpView.as_view(), name='account_signup'),
+    path('login/', Login.as_view(), name='account_login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('search/', ProfileSearch.as_view(), name='profile_search'),
     path('search/search_result/', profile_search, name='profile_search_result'),
     path('<int:pk>/subscribers/subscribers_search_result/', subscribers_search, name='subscribers_search_result'),
