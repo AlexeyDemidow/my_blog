987ac6aefbc31728fe4aeefec10e01c734be3144
AlexeyDemidow <usernzt@gmail.com>
2026-02-15 15:15:22 +0100

    Clean up code in posts/urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,6 @@
-from django.conf import Settings
 from django.urls import path
 
-from posts.views import PostList, PostCreate, like_unlike_post, add_comment, del_comment, post_delete, \
-    like_unlike_comment, repost, post_detail, get_data_for_repost, PostUpdate, pag_home_posts, pag_profile_posts, \
-    update_comment, user_dialogs
-from users.views import SignUpView, Login, Profile, UpdateProfile, ProfileSettings
-from django.contrib.auth import views
+from posts.views import *
 
 urlpatterns = [
     path('update/<int:pk>/', PostUpdate.as_view(), name='post_update'),
@@ -13,7 +8,6 @@ urlpatterns = [
     path('like_unlike/<int:pk>/', like_unlike_post, name='like_unlike_post'),
     path('repost/<int:pk>/', repost, name='repost'),
     path('dialogs_list/', user_dialogs, name='user_dialogs'),
-
     path('get_data_for_repost/<int:pk>/', get_data_for_repost, name='get_data_for_repost'),
     path('like_unlike_comment/<int:pk>/', like_unlike_comment, name='like_unlike_comment'),
     path('add_comment/<int:pk>/', add_comment, name='add_comment'),
@@ -23,6 +17,5 @@ urlpatterns = [
     path('delete/<int:pk>/', post_delete, name='post_delete'),
     path('paginate_home/', pag_home_posts, name='paginate_home_posts'),
     path('paginate_profile/<int:pk>/', pag_profile_posts, name='paginate_profile_posts'),
-
 
 ]
