bdda675ef131c7de8c0e4e7c80108620378b9cb9
AlexeyDemidow <usernzt@gmail.com>
2026-02-17 21:18:17 +0100

    Some fixes in asgi.py for Docker

--- my_blog/asgi.py
+++ my_blog/asgi.py
@@ -1,16 +1,17 @@
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_blog.settings')
+
+django_asgi_app = get_asgi_application()
+
 import chat.routing
 
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
 })
